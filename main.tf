e4c7893a54c98a6f6214cc918dd9acb87d9f0013
Larry Steck <lsteck@us.ibm.com>
2021-12-14 10:58:21 -0600

    add console-link
    Signed-off-by: Larry Steck <lsteck@us.ibm.com>

--- main.tf
+++ main.tf
@@ -8,20 +8,20 @@ locals {
   password_file      = "${local.tmp_dir}/gitea-password.val"
   gitea_username     = var.gitea_username
   gitea_password     = var.gitea_password == "" ? random_password.password.result : var.gitea_password
-  instance_namespace = var.instance_namespace 
+  instance_namespace = var.instance_namespace
   instance_name      = var.instance_name
 
-  gitea_operator_values       = {
+  gitea_operator_values = {
     global = {
-      clusterType = var.cluster_type
-      olmNamespace = var.olm_namespace
+      clusterType       = var.cluster_type
+      olmNamespace      = var.olm_namespace
       operatorNamespace = var.operator_namespace
     }
 
     ocpCatalog = {
-      source = "redhat-gpte-gitea"
+      source  = "redhat-gpte-gitea"
       channel = "stable"
-      name = "gitea-operator"
+      name    = "gitea-operator"
     }
   }
   gitea_operator_values_file = "${local.tmp_dir}/values-gitea-operator.yaml"
@@ -32,9 +32,9 @@ locals {
       clusterType = var.cluster_type
     }
     giteaInstance = {
-      name = local.instance_name
-      namespace = local.instance_namespace
-      giteaAdminUser = local.gitea_username
+      name               = local.instance_name
+      namespace          = local.instance_namespace
+      giteaAdminUser     = local.gitea_username
       giteaAdminPassword = "${local.gitea_password}"
     }
   }
@@ -48,7 +48,7 @@ resource "random_password" "password" {
 }
 
 
-resource null_resource cluster_version {
+resource "null_resource" "cluster_version" {
   provisioner "local-exec" {
     command = "${path.module}/scripts/get-cluster-version.sh ${local.version_file}"
 
@@ -58,13 +58,13 @@ resource null_resource cluster_version {
   }
 }
 
-data local_file cluster_version {
+data "local_file" "cluster_version" {
   depends_on = [null_resource.cluster_version]
 
   filename = local.version_file
 }
 
-resource null_resource print_version {
+resource "null_resource" "print_version" {
   provisioner "local-exec" {
     command = "echo 'Cluster version: ${local.version_re}'"
   }
@@ -74,26 +74,26 @@ resource null_resource print_version {
   }
 }
 
-resource null_resource gitea_operator_helm {
+resource "null_resource" "gitea_operator_helm" {
 
   triggers = {
-    namespace = var.operator_namespace
-    name = "gitea-operator"
-    chart = "gitea-operator"
-    repository = "https://charts.cloudnativetoolkit.dev"
+    namespace           = var.operator_namespace
+    name                = "gitea-operator"
+    chart               = "gitea-operator"
+    repository          = "https://charts.cloudnativetoolkit.dev"
     values_file_content = yamlencode(local.gitea_operator_values)
-    kubeconfig = var.cluster_config_file
-    tmp_dir = local.tmp_dir
+    kubeconfig          = var.cluster_config_file
+    tmp_dir             = local.tmp_dir
   }
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/deploy-helm.sh ${self.triggers.namespace} ${self.triggers.name} ${self.triggers.chart}"
 
     environment = {
-      KUBECONFIG = self.triggers.kubeconfig
-      REPO = self.triggers.repository
+      KUBECONFIG          = self.triggers.kubeconfig
+      REPO                = self.triggers.repository
       VALUES_FILE_CONTENT = self.triggers.values_file_content
-      TMP_DIR = self.triggers.tmp_dir
+      TMP_DIR             = self.triggers.tmp_dir
     }
   }
 
@@ -103,22 +103,22 @@ resource null_resource gitea_operator_helm {
     command = "${path.module}/scripts/destroy-helm.sh ${self.triggers.namespace} ${self.triggers.name} ${self.triggers.chart}"
 
     environment = {
-      KUBECONFIG = self.triggers.kubeconfig
+      KUBECONFIG          = self.triggers.kubeconfig
       VALUES_FILE_CONTENT = self.triggers.values_file_content
-      TMP_DIR = self.triggers.tmp_dir
+      TMP_DIR             = self.triggers.tmp_dir
     }
   }
 }
 
-resource null_resource wait_gitea_operator_deployment {
+resource "null_resource" "wait_gitea_operator_deployment" {
   depends_on = [null_resource.gitea_operator_helm]
 
   triggers = {
-    namespace = var.operator_namespace
-    name = "gitea-operator"
+    namespace  = var.operator_namespace
+    name       = "gitea-operator"
     kubeconfig = var.cluster_config_file
-    tmp_dir = local.tmp_dir
-    openshift = local.openshift_gitops
+    tmp_dir    = local.tmp_dir
+    openshift  = local.openshift_gitops
   }
 
   provisioner "local-exec" {
@@ -126,33 +126,33 @@ resource null_resource wait_gitea_operator_deployment {
 
     environment = {
       KUBECONFIG = self.triggers.kubeconfig
-      TMP_DIR = self.triggers.tmp_dir
+      TMP_DIR    = self.triggers.tmp_dir
     }
   }
 
 }
 
-resource null_resource gitea_instance_helm {
+resource "null_resource" "gitea_instance_helm" {
   depends_on = [null_resource.wait_gitea_operator_deployment]
-  
+
   triggers = {
-    namespace = local.instance_namespace
-    name = local.instance_name
-    chart = "gitea-instance"
-    repository = "https://charts.cloudnativetoolkit.dev"
+    namespace           = local.instance_namespace
+    name                = local.instance_name
+    chart               = "gitea-instance"
+    repository          = "https://charts.cloudnativetoolkit.dev"
     values_file_content = yamlencode(local.gitea_instance_values)
-    kubeconfig = var.cluster_config_file
-    tmp_dir = local.tmp_dir
+    kubeconfig          = var.cluster_config_file
+    tmp_dir             = local.tmp_dir
   }
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/deploy-helm.sh ${self.triggers.namespace} ${self.triggers.name} ${self.triggers.chart}"
 
     environment = {
-      KUBECONFIG = self.triggers.kubeconfig
-      REPO = self.triggers.repository
+      KUBECONFIG          = self.triggers.kubeconfig
+      REPO                = self.triggers.repository
       VALUES_FILE_CONTENT = self.triggers.values_file_content
-      TMP_DIR = self.triggers.tmp_dir
+      TMP_DIR             = self.triggers.tmp_dir
     }
   }
 
@@ -162,23 +162,23 @@ resource null_resource gitea_instance_helm {
     command = "${path.module}/scripts/destroy-helm.sh ${self.triggers.namespace} ${self.triggers.name} ${self.triggers.chart}"
 
     environment = {
-      KUBECONFIG = self.triggers.kubeconfig
-      REPO = self.triggers.repository
+      KUBECONFIG          = self.triggers.kubeconfig
+      REPO                = self.triggers.repository
       VALUES_FILE_CONTENT = self.triggers.values_file_content
-      TMP_DIR = self.triggers.tmp_dir
+      TMP_DIR             = self.triggers.tmp_dir
     }
   }
 }
 
-resource null_resource wait_gitea_instance_deployment {
+resource "null_resource" "wait_gitea_instance_deployment" {
   depends_on = [null_resource.gitea_instance_helm]
 
   triggers = {
-    namespace = local.instance_namespace
-    name = local.instance_name
+    namespace  = local.instance_namespace
+    name       = local.instance_name
     kubeconfig = var.cluster_config_file
-    tmp_dir = local.tmp_dir
-    openshift = local.openshift_gitops
+    tmp_dir    = local.tmp_dir
+    openshift  = local.openshift_gitops
   }
 
   provisioner "local-exec" {
@@ -186,8 +186,28 @@ resource null_resource wait_gitea_instance_deployment {
 
     environment = {
       KUBECONFIG = self.triggers.kubeconfig
-      TMP_DIR = self.triggers.tmp_dir
+      TMP_DIR    = self.triggers.tmp_dir
     }
   }
+}
 
+resource "null_resource" "gitea_consolelink_deployment" {
+  depends_on = [null_resource.wait_gitea_instance_deployment]
+
+  triggers = {
+    namespace  = local.instance_namespace
+    name       = local.instance_name
+    kubeconfig = var.cluster_config_file
+    tmp_dir    = local.tmp_dir
+    openshift  = local.openshift_gitops
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/create-console-link.sh ${self.triggers.namespace} ${self.triggers.name} ${self.triggers.openshift}"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
+      TMP_DIR    = self.triggers.tmp_dir
+    }
+  }
 }
